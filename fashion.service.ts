2958d050f99299701f4c42737b972d5d33760c96
tuytank <anhtt22411c@st.uel.edu.vn>
2025-03-11 02:11:21 +0700

    Add files via upload

--- fashion.service.ts
+++ fashion.service.ts
@@ -2,31 +2,23 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Fashion } from './models/fashion.model';
+
 @Injectable({
   providedIn: 'root'
 })
 export class FashionService {
-  private apiUrl = 'http://localhost:4000/api/fashion';
+  private apiUrl = 'http://localhost:4000/api/fashion'; // API của server
 
   constructor(private http: HttpClient) {}
 
+  // Lấy toàn bộ danh sách Fashion
   getFashions(): Observable<Fashion[]> {
     return this.http.get<Fashion[]>(this.apiUrl);
   }
 
+  // Lấy một Fashion theo ID
   getFashionById(id: string): Observable<Fashion> {
     return this.http.get<Fashion>(`${this.apiUrl}/${id}`);
   }
-
-  addFashion(fashion: Fashion): Observable<Fashion> {
-    return this.http.post<Fashion>(this.apiUrl, fashion);
-  }
-
-  updateFashion(id: string, fashion: Fashion): Observable<Fashion> {
-    return this.http.put<Fashion>(`${this.apiUrl}/${id}`, fashion);
-  }
-
-  deleteFashion(id: string): Observable<void> {
-    return this.http.delete<void>(`${this.apiUrl}/${id}`);
-  }
 }
+
